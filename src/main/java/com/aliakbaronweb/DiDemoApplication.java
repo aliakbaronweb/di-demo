4ca63c84285d2777b1174f13f0828869737ef68f
Ali Akbar <ali.zahra.nice@gmail.com>
2018-09-04 20:45:25 +0430

    027: Dependency Injection with the Spring Framework The Spring Framework Context

--- src/main/java/com/aliakbaronweb/DiDemoApplication.java
+++ src/main/java/com/aliakbaronweb/DiDemoApplication.java
@@ -1,12 +1,16 @@
 package com.aliakbaronweb;
 
+import com.aliakbaronweb.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class DiDemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DiDemoApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(DiDemoApplication.class, args);
+        MyController myController = (MyController) context.getBean("myController");
+        myController.hello();
     }
 }
---
+++ src/main/java/com/aliakbaronweb/controllers/MyController.java
@@ -0,0 +1,13 @@
+package com.aliakbaronweb.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+    public String hello() {
+        System.out.println("hello !!!");
+        return "foo";
+    }
+
+}
